bf119062dacd192f5e09b0972bd513d001a4ebeb
Anmar Abdi <anmarabdi@yahoo.com>
2022-04-07 10:10:08 -0500

    signup success fixed

--- signup.php
+++ signup.php
@@ -5,7 +5,9 @@
 <main>
 <h1>Signup</h1>
 <?php
- if (isset($_GET["error"]/*, $_GET["signup"]*/)) {
+
+
+ if (isset($_GET["error"])) {
    if ($_GET["error"] == "emptyfields") {
      echo '<p class="signuperror">Fill in all fields!</p>';
    }
@@ -24,20 +26,28 @@
    else if ($_GET["error"] == "usertaken") {
     echo '<p class="signuperror">Username is already taken!</p>';
   }
- } /*else if ($_GET["signup"] == "success") {
-  echo '<p>Signup successful!</p>'; */
  }
- 
+ if (isset($_GET["signup"])) {
+  if ($_GET["signup"] == "success") {
+  echo '<p>Signup successful!</p>'; 
+ }
+}
 ?>
-<form action="includes/signup.inc.php" method="post">
-  <div class="form-group">
-    <input type="text" name="uid" placeholder="Username">
-    <input type="text" name="mail" placeholder="Email">
-    <input type="password" name="pwd" placeholder="Password">
-    <input type="password" name="pwd-repeat" placeholder="Repeat password">
+<div class="container d-flex justify-content-center">
+  <div class="row">
+    <div class="card">
+      <form action="includes/signup.inc.php" method="post">
+        <div class="form-group">
+          <input type="text" name="uid" placeholder="Username">
+          <input type="text" name="mail" placeholder="Email">
+          <input type="password" name="pwd" placeholder="Password">
+          <input type="password" name="pwd-repeat" placeholder="Repeat password">
+        </div>
+        <button type="submit" name="signup-submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
   </div>
-  <button type="submit" name="signup-submit" class="btn btn-primary">Submit</button>
-</form>
+</div>
 </main>
 <!--
 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
